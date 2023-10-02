0f63faacb4bbfb25aadf70eeba07d66a751f563c
Douglass Coronado <dcoronado@funio.com>
2023-10-02 19:35:30 -0400

    force lower case in name

--- kubernetes_pool.tf
+++ kubernetes_pool.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_kubernetes_cluster_node_pool" "myk8spool" {
   for_each              = azurerm_kubernetes_cluster.myk8s
-  name                  = "${each.key}"
+  name                  = lower("${each.key}")
   kubernetes_cluster_id = each.value.id
   vm_size               = "Standard_DS2_v2"
   node_count            = 1
