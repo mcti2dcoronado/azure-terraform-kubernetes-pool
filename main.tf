10a55c1897080b35fa476a5fc5f2e97dad574066
Douglass Coronado <dcoronado@funio.com>
2023-10-02 19:18:14 -0400

    Add local variables

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 locals {
-  cluster_names = ["Pacific","Atlantic","Indian","Artic"]
+  cluster_names = ["Europo","Io","Elara","Ganymede","Callisto"]
+
+    backend_address_pool_name      = "${azurerm_virtual_network.vnetmcit.name}-beap"
+    frontend_port_name             = "${azurerm_virtual_network.vnetmcit.name}-feport"
+    frontend_ip_configuration_name = "${azurerm_virtual_network.vnetmcit.name}-feip"
+    http_setting_name              = "${azurerm_virtual_network.vnetmcit.name}-be-htst"
+    listener_name                  = "${azurerm_virtual_network.vnetmcit.name}-httplstn"
+    request_routing_rule_name      = "${azurerm_virtual_network.vnetmcit.name}-rqrt"
+    redirect_configuration_name    = "${azurerm_virtual_network.vnetmcit.name}-rdrcfg"
 }
 
 
@@ -8,20 +16,4 @@ resource "azurerm_resource_group" "mcit" {
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "myk8s" {
-  for_each            = { for cluster in local.cluster_names : cluster => cluster }
-  name                = "k8s-${var.convention}-${each.key}"
-  location            = azurerm_resource_group.mcit.location
-  resource_group_name = azurerm_resource_group.mcit.name
-  dns_prefix          = "${var.prefix}-k8s"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
